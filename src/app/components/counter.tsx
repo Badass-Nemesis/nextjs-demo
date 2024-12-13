6991d1ddc131b8ad0b9006041d560ea2a112680a
Badass-Nemesis <harshitanant29@gmail.com>
2024-12-14 02:31:29 +0530

    added useAuth and useUser hooks in counter component which is a client component because we can't just await currentUser() and auth() directly in a client component

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -1,10 +1,21 @@
 'use client'
 
 import { useState } from "react";
+import { useAuth, useUser } from "@clerk/nextjs";
 
 export const Counter = () => {
+    // const { isLoaded, userId, sessionId, getToken } = useAuth();
+    const { isLoaded, isSignedIn, user } = useUser();
+
     console.log("Counter Component");
     const [count, setCount] = useState<number>(0);
+
+    // if (!isLoaded || !userId) {
+    //     return null;
+    // }
+    if (!isLoaded || !isSignedIn) {
+        return null;
+    }
 
     return <button onClick={() => setCount(count + 1)} className="bg-red-600 px-4 py-2 rounded-xl">
         Clicked button {count} times
